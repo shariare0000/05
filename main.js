9eab8f393fd260a651bceecd0e63eed65846fadc
shariare0000 <jashan432578@gmail.com>
2025-11-16 18:52:00 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,200 @@
+// document.getElementById('b5').innerHTML="Our first javascript code";
+// document.write("Hello world!!!!!!!!!!");
+// window.alert("Welcome to my website");
+
+// console.log("Our Js finished");
+
+
+// single line comment
+
+/* multi line comment */
+
+// let $num1=1;
+
+// let _num2 =2;
+//  var num = 5;
+//  const value =3.1415;
+//   number =34;
+//   Number=36;
+
+
+// difference among let var and const
+
+// var num1 =25;
+// console.log(num1);
+//  var num1= 30;
+//  num1= 40;
+//  console.log(num1);
+
+// let num2 = 30;
+// console.log(num2);
+//  num2 = 40;
+// console.log(num2);
+
+// 
+
+// {
+//     let number = 25;
+// console.log(number);
+// }
+
+
+// {
+//     var number12 = 30;
+// }
+
+// console.log(number13);
+
+// let firstName;
+// let lastName= "Mim";
+// let number = 12;
+// let number2 = "24";
+// let number3 = 24.566;
+// let number4 =121342432423423424324432786768n;
+// let isBangladeshi =true;
+// let isIndian = false;
+// let person={
+//     firstName:"Aishy",
+// }
+
+// let value = null; 
+
+// console.log(typeof(number3));
+
+// let firstName = "Rafiqul";
+
+// let lastName = " Rana";
+
+// let fullName = firstName + lastName;
+// console.log(fullName);
+
+// let x = 5;
+// let y= 3;
+// // console.log(x=x+y);
+// console.log(x<=y);
+
+
+// let age = 21;
+// let text1 = "You can  drive!";
+// let text2 = "You can Not drive!";
+
+// if (age >= 18) {
+//  console.log(text1);
+// }else{
+//     console.log(text2);
+    
+// }
+
+
+// let mark = 100;
+// if (mark >= 80){
+//  console.log("Your grade is A+");
+// }else if(mark >= 70){
+//     console.log("Your grade is A");
+// }else if(mark >= 60){
+//     console.log("Your grade is A-");
+// }else if(mark >= 50){
+//     console.log("Your grade is B");
+// }else if(mark >= 40){
+//     console.log("Your grade is C");
+// }else if(mark >= 33){
+//     console.log("Your grade is D");
+// }else{
+//    console.log("Your grade is F"); 
+// }
+
+// switch
+
+// let score =10;
+// switch(true){
+//     case(score >=80 && score<=100):
+//     grade = "A+";
+//     console.log(grade);
+//     break;
+//     case(score >=70 && score<=79):
+//     grade = "A";
+//     console.log(grade);
+//     break;
+//     case(score >=60 && score<=69):
+//     grade = "A-";
+//     console.log(grade);
+//     break;
+//     case(score >=50 && score<=59):
+//     grade = "B";
+//     console.log(grade);
+//     break;
+//     case(score >=40 && score<=49):
+//     grade = "C";
+//     console.log(grade);
+//     break;
+//     case(score >=33 && score<=39):
+//     grade = "D";
+//     console.log(grade);
+//     break;
+//     case(score >=0 && score<=32):
+//     grade = "F";
+//     console.log(grade);
+//     break;
+//     default:
+//      grade="Invalid score";
+//      console.log(grade);
+// }
+
+//  ternary operator ? 
+
+// let age = 18;
+// age>=18 ? console.log("You are eligible for vote") : console.log("You are not  eligible for vote");
+
+//  for loop
+// let sum = 0;
+// for(let i=1; i<=10; i++){
+//     // console.log(`${i}.jony`);
+//     sum = sum + i; 
+//     console.log(sum);     
+// }
+
+//  while loop
+
+// let i= 0;
+// while(i<=10){
+//    console.log(`${i}.elias`); backtik
+//    i++; 
+// }
+
+//  do while loop
+
+// let i = 0;
+// do{
+//  console.log(`${i}.Mim`); 
+//  i++;  
+// }
+// while(i<=10);
+
+
+// let userName= aishi.firstname;
+// let password= aishi.password;
+// if(userName === "Web developer batch-5" && password === "12345678"){
+// console.log("successfully login");
+// }else{
+//    console.log(" login failed"); 
+// }
+
+//  Nan  
+// let age;
+// console.log(typeof(age));
+
+// let num1= 12;
+//  let num2= 0;
+//  result = num1/num2;
+
+
+//  console.log(0/0);
+
+let num1= 12;
+ let num2 ="4";
+ console.log(num1+num2);
+ 
+ let name1= "Mim";
+ let name2= "Dola";
+ console.log(name1+name2);
+ 
